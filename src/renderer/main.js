1fb19d85f782a809ebdb0d2f940c0b5772c1a3d8
sixteen <yinyazuolun@gmail.com>
2021-02-22 22:53:33 +0800

    加入ui库

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -3,10 +3,14 @@ import axios from 'axios'
 
 import App from './App'
 import router from './router'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
 Vue.http = Vue.prototype.$http = axios
 Vue.config.productionTip = false
+
+Vue.use(ElementUI);
 
 /* eslint-disable no-new */
 new Vue({
